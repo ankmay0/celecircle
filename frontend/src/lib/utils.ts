0cfee3ae7175fed7f17d743dab394907b3230324
Omprakash Sahoo <irajvmailcom0@gmail.com>
2026-03-10 15:18:25 +0530

    changed frontend codes and format and updated the codes

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export const BACKEND_URL = 'https://celecircle.onrender.com'
+export const BACKEND_URL = import.meta.env.DEV ? '' : 'https://celecircle.onrender.com'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
